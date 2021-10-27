d6911b37843a7325d0d6f2ba3d05cdce31dd9a84
artem kopan <artem.kopan@poynt.co>
2021-10-27 18:36:45 +0300

    remove classes

--- app/src/main/java/io/test/app/presentation/NewsActivity.kt
+++ app/src/main/java/io/test/app/presentation/NewsActivity.kt
@@ -1,26 +1,12 @@
 package io.test.app.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.coroutineScope
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import io.test.app.R
-import io.test.app.dependencies.ServiceLocator
-import kotlinx.coroutines.flow.consumeAsFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 
 class NewsActivity : AppCompatActivity() {
-
-    private val viewModel by lazy(LazyThreadSafetyMode.NONE) {
-        ViewModelProvider(
-            this,
-            ServiceLocator.getNewsViewModelFactory()
-        )[NewsViewModel::class.java]
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,23 +14,6 @@ class NewsActivity : AppCompatActivity() {
         val adapter = ArticleAdapter()
         findViewById<RecyclerView>(R.id.news_recycler).apply {
             this.adapter = adapter
-            addItemDecoration(ArticleItemDecoration(resources))
-            addItemDecoration(
-                DividerItemDecoration(
-                    this@NewsActivity,
-                    DividerItemDecoration.VERTICAL
-                )
-            )
         }
-        viewModel
-            .newsItems
-            .onEach { adapter.submitList(it) }
-            .launchIn(lifecycle.coroutineScope)
-
-        viewModel
-            .errorChannel
-            .consumeAsFlow()
-            .onEach { Toast.makeText(this, it.message.orEmpty(), Toast.LENGTH_LONG).show() }
-            .launchIn(lifecycle.coroutineScope)
     }
 }
