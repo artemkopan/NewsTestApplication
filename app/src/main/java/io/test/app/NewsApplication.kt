f8068b35d0224e5fd129f7206c4d7cf8d8d5317d
artem kopan <artem.kopan@poynt.co>
2021-11-01 17:16:39 +0200

    remove unused imports

--- app/src/main/java/io/test/app/NewsApplication.kt
+++ app/src/main/java/io/test/app/NewsApplication.kt
@@ -1,12 +1,10 @@
 package io.test.app
 
 import android.app.Application
-import io.test.app.dependencies.ServiceLocator
 
 class NewsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        ServiceLocator.init(this)
     }
 }
